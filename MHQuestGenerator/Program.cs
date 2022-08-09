f74868b0a3dedc633e9a47e45be80dc817f76dd3
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2022-08-09 12:08:15 +1200

    Added DI for dbContext

--- MHQuestGenerator/Program.cs
+++ MHQuestGenerator/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MHQuestGenerator.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// DB context is registered through dependency injection
+builder.Services.AddDbContext<QuestContext>(opt =>
+    opt.UseInMemoryDatabase("Quest"));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
