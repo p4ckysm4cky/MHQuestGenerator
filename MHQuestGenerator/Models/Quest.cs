6fd97f2b5b7403284a8de89c6f5db61514904af8
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2022-08-09 12:03:10 +1200

    Added models

---
+++ MHQuestGenerator/Models/Quest.cs
@@ -0,0 +1,10 @@
+﻿namespace MHQuestGenerator.Models
+{
+    public class Quest
+    {
+        public long Id { get; set; }
+        public string Monster { get; set; }
+        public string ArmorSet { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
---
+++ MHQuestGenerator/Models/QuestContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace MHQuestGenerator.Models
+{
+    public class QuestContext: DbContext
+    {
+        public QuestContext(DbContextOptions<QuestContext> options) : base(options) { }
+        public DbSet<Quest> Quest { get; set; } = null!;
+    }
+}
