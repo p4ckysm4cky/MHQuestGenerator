857f52c80beacfdc1de7310299f921c32a922a13
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2022-08-09 12:27:04 +1200

    Added template controller

---
+++ MHQuestGenerator/Controllers/QuestsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MHQuestGenerator.Models;
+
+namespace MHQuestGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestsController : ControllerBase
+    {
+        private readonly QuestContext _context;
+
+        public QuestsController(QuestContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Quests
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Quest>>> GetQuest()
+        {
+          if (_context.Quest == null)
+          {
+              return NotFound();
+          }
+            return await _context.Quest.ToListAsync();
+        }
+
+        // GET: api/Quests/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Quest>> GetQuest(long id)
+        {
+          if (_context.Quest == null)
+          {
+              return NotFound();
+          }
+            var quest = await _context.Quest.FindAsync(id);
+
+            if (quest == null)
+            {
+                return NotFound();
+            }
+
+            return quest;
+        }
+
+        // PUT: api/Quests/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuest(long id, Quest quest)
+        {
+            if (id != quest.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(quest).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Quests
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Quest>> PostQuest(Quest quest)
+        {
+          if (_context.Quest == null)
+          {
+              return Problem("Entity set 'QuestContext.Quest'  is null.");
+          }
+            _context.Quest.Add(quest);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuest", new { id = quest.Id }, quest);
+        }
+
+        // DELETE: api/Quests/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuest(long id)
+        {
+            if (_context.Quest == null)
+            {
+                return NotFound();
+            }
+            var quest = await _context.Quest.FindAsync(id);
+            if (quest == null)
+            {
+                return NotFound();
+            }
+
+            _context.Quest.Remove(quest);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool QuestExists(long id)
+        {
+            return (_context.Quest?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
